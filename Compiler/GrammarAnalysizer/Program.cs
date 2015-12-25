7aa7898bb77287fd811422feb6a0dd54bf34abb0
步清 舒 <monkey_d_asce@foxmail.com>
2015-12-25 21:42:13 +0800

    fix basic type and assignment bugs

--- Compiler/GrammarAnalysizer/Program.cs
+++ Compiler/GrammarAnalysizer/Program.cs
@@ -13,8 +13,8 @@ namespace Zodiac
             //string code = "var i,console = long(1*1+1), IO; console.write(i+1);";
             //string code = " var i,j = 1+1+1+1, 1+2+3; ";
             // string code = " var i = long(1); ";
-            //string code = System.IO.File.ReadAllText("../../../test/while_statement.zs");
-            string code = System.IO.File.ReadAllText(args[0]);
+            string code = System.IO.File.ReadAllText("../../../test/basic_type.zs");
+            //string code = System.IO.File.ReadAllText(args[0]);
             //string code = "static func getAB : long { param a = long; param b = long; param c = long; return b;} var i = getAB(1,2,3);";
             // string code = "var i = 111; var j = i.ToString()[2];";
             grammarAnalysizer.ParseSample(code);
@@ -24,7 +24,7 @@ namespace Zodiac
                 var s = grammarAnalysizer.ParseTree.ParserMessages;
                 foreach ( var error in s)
                 {
-                    Console.WriteLine("("+error.Location.Line+","+error.Location.Column+")\t" + error.ToString());
+                    Console.WriteLine("("+(error.Location.Line+1)+","+error.Location.Column+")\t" + error.ToString());
                 }
                 return;
             }
---
+++ Compiler/test/basic_type.zs
@@ -0,0 +1,18 @@
+var s = "sss";
+IO.writeln(s);
+var ch = 'a';
+IO.writeln(ch);
+var r = 4.44;
+var bo = False;
+var l = long(0);
+bo ,r = True, 5;
+IO.writeln(r); 
+if (bo)
+{
+    IO.writeln("bo is true");
+}
+else
+{
+    IO.writeln("bo is false");
+}
+
